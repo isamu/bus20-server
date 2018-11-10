bda8bca6a061cc96a2d4d6b0aa553579baa68204
Isamu Arimoto <isamu@to-kyo.to>
2018-11-11 05:39:03 +0900

    clean kml parser

--- src/models/index.js
+++ src/models/index.js
@@ -3,12 +3,14 @@ const Node = require("./Node");
 const Edge = require("./Edge");
 
 const parser = require("./parser");
+const kml_parser = require("./kml_parser.js");
 
 const models = {
   Graph,
   Node,
   Edge,
   parser,
+  kml_parser,
 }
 
 module.exports = models;
